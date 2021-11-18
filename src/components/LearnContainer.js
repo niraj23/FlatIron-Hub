beaae77ad00250affbf3d3f66eae984901831b6a
louis jacob <loujacob2@gmail.com>
2021-11-18 18:27:40 -0500

    update

--- src/components/LearnContainer.js
+++ src/components/LearnContainer.js
@@ -4,38 +4,31 @@ import AddLearn from "./AddLearn"
 import Search from "./Search";
 import {useState} from 'react';
 import {useEffect } from 'react';
-import background from "../images/background.jpg"
-
 function LearnContainer() {
     const[ learnItems, setLearnItems ] = useState ([]);
     const[ searchTerm, setSearchTerm] = useState ('')
-    
     useEffect(() => {
-        fetch("http://localhost:8000/learnitems")
+        fetch("http://localhost:3000/learnitems")
         .then ((res) => res.json())
         .then ((learnItemsArray ) => {
             setLearnItems (learnItemsArray)
         })
     }, []);
-
     function handleAddLearnItems(newLearnItem) {
-        const updatedLearnItemsArray = [... learnItems, newLearnItem];
+        const updatedLearnItemsArray = [...learnItems, newLearnItem];
         setLearnItems(updatedLearnItemsArray);
     }
-
     const displayedLearnItems = learnItems.filter((learnItem) =>{
         return learnItem.description.toLowerCase().includes(searchTerm.toLowerCase())
     });
-
     return (
-        <div className= "LearnContainer"
-        style = {{backgroundImage: `url(${background})`}}>
-            <Search searchTerm = {searchTerm} onSearchChange = {setSearchTerm} />
+        <div className= "LearnContainer">
+            <br /> <br />
             <AddLearn onAddLearn = {handleAddLearnItems}/>
-            <LearnList 
+            <Search searchTerm = {searchTerm} onSearchChange = {setSearchTerm} />
+            <LearnList
             learnItems = {displayedLearnItems}/>
         </div>
     )
 };
-
 export default LearnContainer
