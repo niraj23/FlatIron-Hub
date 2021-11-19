ecfca0f6fbec38e5f992c1433fc088d5cb168d15
mtoth003 <89261754+mtoth003@users.noreply.github.com>
2021-11-19 10:06:57 -0500

    Add files via upload

---
+++ components/GamesPagination.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const GamesPagination = ({gamesPerPage, totalGames, paginate}) => {
+    const pageNumbers = []
+    for(let i = 1; i <= Math.ceil(totalGames / gamesPerPage); i++){
+        pageNumbers.push(i)
+    }
+    return (
+        <PaginationContainer>
+    
+            <ul className="pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className="page-item">
+                        <a onClick={() => paginate(number)} href="!#" className="page-link">{number}</a>
+                    </li>
+                ))}
+            </ul>
+        </PaginationContainer>
+    )
+}
+
+export default GamesPagination
+
+const PaginationContainer = styled.div`
+    display: inline-block
+`
+
